f1515dbe89f1ea5f741ca2a2adc9433f98fbd158
Paras Mendiratta <paras@ezibyte.com>
2018-09-25 00:41:03 +0530

    fixed reference issue

--- lib/index.ts
+++ lib/index.ts
@@ -1,17 +1,1 @@
-import { rejects } from "assert";
-
-export function echo(): boolean {
-    return true
-}
-
-export function echoWithPromise(): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-        return resolve(true)
-    })
-}
-
-export function echoWithFailedPromise(): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-        return reject(false)
-    })
-}
+export * from './token-generator/token-generator';
